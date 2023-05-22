3e5dfbf67a3a76b3b3925947e59f49ec29b56ca8
Aung662001 <107274438+Aung662001@users.noreply.github.com>
2023-05-23 00:37:35 +0630

    change a little bit to routers

--- src/pages/api/app/index.tsx
+++ src/pages/api/app/index.tsx
@@ -6,20 +6,40 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const email = req.query.email as string;
+
+  //user
   const user = await prisma.users.findFirst({
     where: { email: email },
   });
   const company_id = user?.companies_id as number;
+  //location
   const location = await prisma.locations.findMany({
     where: {
       companies_id: company_id,
     },
   });
-  const menus = await prisma.menus.findMany();
+  const locationIds = location.map((location) => location.id as number);
+  //menuLocations
+  const menusLocations = await prisma.menus_locations.findMany({
+    where: {
+      locations_id: {
+        in: locationIds,
+      },
+    },
+  });
+  const menuIds = menusLocations.map((ml) => ml.menus_id);
+  //menus
+  const menus = await prisma.menus.findMany({
+    where: {
+      id: {
+        in: menuIds,
+      },
+    },
+  });
+  console.log(menus);
   const addon = await prisma.addons.findMany();
   const addonCategories = await prisma.addon_categories.findMany();
   const menuCategories = await prisma.menu_categories.findMany();
-  const menusLocations = await prisma.menus_locations.findMany();
   const company = await prisma.companies.findMany({
     where: {
       id: company_id,
